f7d6ebdf98526014d93efd5fd7b5f48815f3693a
ARCHANAGOPI <43091924+ARCHANAGOPI@users.noreply.github.com>
2019-07-26 11:04:16 +0530

    aaa

--- src/test/java/cs4/runner.java
+++ src/test/java/cs4/runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/feature/cs4.feature")
+@CucumberOptions(features="src/test/resources/feature/cs4.feature",plugin="json:target\\jsonreport.json")
 public class runner {
 
 }
